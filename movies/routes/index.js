0acce67fb285f085a5271a81aab1001ca44348d7
Fred Lopez <fred@fredlopez.com>
2021-03-18 12:12:45 -0700

    added user login - individual list that can be added and subtracted from

--- movies/routes/index.js
+++ movies/routes/index.js
@@ -5,12 +5,42 @@ const { v4: uuidv4 } = require('uuid');
 
 
 router.get('/', (req, res) => {
+    if(req.session) {
+        req.session.username = "maybe"
+        req.session.mycat = "we wee"
+    }
     res.render("index", {movies: movies})
 })
 
-router.get('/add', (req, res) => {
-    res.render("add")
+router.get('/user', (req, res) => {
+    res.render('user')
 })
+
+router.post('/user', (req, res) => {
+    let name = req.body.name
+    let age = req.body.age
+    req.session.name = name
+    req.session.age = age
+
+    res.redirect("/confirm")
+
+})
+
+router.get('/confirm', (req, res) => {
+    let name = req.session.name
+    let age = req.session.age
+    let random = req.session.mycat
+    
+    res.render('confirm', {name: name, age: age, random: random})
+})
+
+router.get('/test', (req, res) => {
+    let name = req.session.username
+    console.log(name)
+    res.send(name)
+})
+
+
 
 router.get('/details/:id', (req, res) => {
     let id = req.params.id
@@ -37,25 +67,58 @@ router.get('/genre/:genre', (req, res) => {
 
 })
 
-router.post('/add-movie', (req, res) => {
-    let title = req.body.movieTitle
-    let description = req.body.desc
-    let genre = req.body.movieGenre
-    let posterURL = req.body.posterURL
 
-    let newMovie = {
-        title: title,
-        description: description,
-        genre: genre,
-        posterURL: posterURL,
-        movieID: uuidv4()
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.get('/logout', (req, res) => {
+    req.session.destroy(function (err) {
+        res.redirect('/')
+    })
+})
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+router.post('/register', (req, res) => {
+    let name = req.body.name
+    let age = req.body.age
+    let password = req.body.password
+
+    let newUser = {
+        name: name,
+        age: age,
+        password: password,
+        creator: uuidv4()
     }
 
-    movies.push(newMovie)
+    users.push(newUser)
+    console.log(users)
 
     res.redirect('/')
 })
 
+router.post('/login', (req, res) => {
+    const name = req.body.name
+    const password = req.body.password
+
+    const userCheck = users.find((user) => {
+        return user.name == name && user.password == password
+    })
+
+    if (userCheck) {
+        req.session.name = name
+        res.redirect("/users")
+    }
+    
+    else { 
+        res.render('login')
+    }
+    
+})
 
 
 module.exports = router
---
+++ movies/routes/users.js
@@ -0,0 +1,66 @@
+
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+    res.render('allMovies', {movies: movies})
+})
+
+router.get('/myMovies', (req, res) => {
+    let userName = req.session.name
+    let user = users.filter((user) => user.name == userName )
+    let sortedMovies = []
+    for( i=0 ; i < user[0].myMovies.length; i++) {
+        let movie = movies.filter((id) => user[0].myMovies[i] == id.movieID)
+        sortedMovies.push(movie[0])
+    }
+    res.render('uMovies', {movies: sortedMovies})
+})
+
+router.get('/movies/:id', (req, res) => {
+    let userName = req.session.name
+    let user = users.filter((user) => user.name == userName )
+    let addMovie = req.params.id
+    user[0].myMovies.push(addMovie)
+    res.redirect('/users')
+})
+
+router.get('/add', (req, res) => {
+    res.render("add")
+})
+
+router.post('/add-movie', (req, res) => {
+    let title = req.body.movieTitle
+    let description = req.body.desc
+    let genre = req.body.movieGenre
+    let posterURL = req.body.posterURL
+
+    let newMovie = {
+        title: title,
+        description: description,
+        genre: genre,
+        posterURL: posterURL,
+        movieID: uuidv4()
+    }
+
+    movies.push(newMovie)
+
+    res.redirect('/')
+})
+
+router.get('/remove/:id', (req, res) => {
+    let userName = req.session.name
+    let user = users.filter((user) => user.name == userName )
+    let removeMovie = req.params.id
+    for (i = 0; i < user[0].myMovies.length; i++) {
+        if(user[0].myMovies[i] == removeMovie) {
+            user[0].myMovies.splice(i, 1)
+            
+        }
+    }
+
+    res.redirect('/users/myMovies')
+})
+
+module.exports = router
